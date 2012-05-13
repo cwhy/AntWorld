121b7ea58af63a3ecf2fe33000912f7950fe2fa0
Chen Yu <owner@CY-T400.(none)>
2012-05-13 16:16:56 +0800

    Slight improvement Try to achieve full image refresh performance increase but failed..(Actually we can just keep it partially refreshing...) Leave the function there in case we need updated gitignore..

--- antWorld.py
+++ antWorld.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 import pygame, sys
 from worldModel import AntWorld
 from math import pi
@@ -23,6 +24,12 @@ def getLandColor(landElement, RANDOM = False):
 		blue = 250-int((landElement.smell[0]/100)*100)
 		return ( red, green, blue ) 
 
+def fullLandUpdate(antWorld):
+	landArray = np.zeros((antWorld.length, antWorld.width, 3), dtype=int) # 3 indicates we need to store R,G and B values for each point
+	for column in antWorld.land.element:
+		for e in column:
+			landArray[e.x, e.y, :] = getLandColor(e)
+	pygame.surfarray.blit_array(SURFACE, landArray)
 
 def drawLandUpdate(ant):
     '''refresh the part of the background that ant went pass'''
@@ -38,7 +45,7 @@ def drawAnts(ant):
 #Initialization
 pygame.init()
 fpsClock = pygame.time.Clock() #setup clock
-antWorld = AntWorld(10, 700,700) # game model
+antWorld = AntWorld(30, 800, 800) # game model
 FPS = 30 # frames per second setting
 iWHITE = (250, 250, 250) # background color
 
@@ -52,10 +59,10 @@ foodImg = pygame.image.load('food.png')
 
 #World Simulation Start
 while not antWorld.checkSuccess(): # the main game loop
-#   SURFACE.fill(iWHITE)
 
     antWorld.run()
     map(drawLandUpdate, antWorld.ants)  
+#   fullLandUpdate(antWorld)
     SURFACE.blit(foodImg, antWorld.getFoodPosition())
     map(drawAnts, antWorld.ants)  
     
